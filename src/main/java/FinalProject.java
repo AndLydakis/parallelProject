8350df9b568f78b4d84e87aa8eb86d86c43a5d3d
ChunkLightTuna <chris@oeleri.ch>
2017-04-02 15:33:04 -0400

    Initial commit

---
+++ src/main/java/FinalProject.java
@@ -0,0 +1,23 @@
+/**
+ * Created by ChunkLightTuna on 4/2/17.
+ */
+public class FinalProject {
+
+
+    /**
+     * @param args doot doot doo
+     */
+    public static void main(String[] args) {
+        System.out.println("dooks");
+
+        State.Board board = new State.Board(20, 20, 20, 10);
+
+        for (int[][] ints : board.cube) {
+            for (int[] anInt : ints) {
+                for (int i : anInt) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/State.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+/**
+ * Created by ChunkLightTuna on 4/2/17.
+ */
+public class State {
+
+
+    public static class Board {
+        final int height;
+        final int width;
+        final int depth;
+        int[][][] cube;
+
+        Board(int width, int height, int depth, int blockHp) {
+            this.width = width;
+            this.height = height;
+            this.depth = depth;
+
+
+            cube = new int[width][height][depth];
+
+
+
+            for (int[][] ints : cube) {
+                for (int[] anInt : ints) {
+                    Arrays.fill(anInt, blockHp);
+                }
+            }
+
+        }
+
+        void getFace(int side) {
+            if (side > 5) {
+                throw new IllegalArgumentException("a cube can only have 6 sides");
+            }
+
+            int[]
+            for (int[][] ints : cube) {
+                for (int[] anInt : ints) {
+                    anInt[width-1]
+                }
+            }
+
+
+        }
+
+
+    }
+}
